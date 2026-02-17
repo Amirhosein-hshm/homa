47c867c86f5e95be1582b52b3f064a090f1195bf
rezahtms <rezahtms@gmail.com>
2026-02-17 16:24:52 +0330

    style(sonner):change sonner position in pages

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { QueryClientProvider } from "@tanstack/react-query";
-import type { ReactNode } from "react";
-import { getQueryClient } from "@/lib/query/get-query-client";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { getQueryClient } from "@/lib/query/get-query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
@@ -12,7 +12,7 @@ export default function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>{children}</TooltipProvider>
-      <Toaster position="top-center" richColors closeButton />
+      <Toaster position="bottom-center" richColors closeButton />
     </QueryClientProvider>
   );
 }
